c4740729c551448fd8b18de166868e402b5f5f19
Gabriel <lsvillegas7@yahoo.com>
2022-05-28 20:40:26 -0700

    to test removing page 1 and using JobHistory

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import JobHistoryPage from './posts/JobHistoryPage'
 
 export default function Home() {
   return (
@@ -11,62 +12,13 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Read{' '}
-          <Link href= "/posts/first-post">
-            <a>First Post</a>
-          </Link>
-        </h1>
-        <h1>
-          <Link href= "/posts/JobHistoryPage">
-            <a>Jobs Page</a>
-          </Link>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+       <JobHistoryPage />
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+        
+
+          
       </footer>
 
       <style jsx>{`
