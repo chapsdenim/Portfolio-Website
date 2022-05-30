fb0eb5ce8c5f9b0d7943bd053901aefec9deaeda
Gabriel <lsvillegas7@yahoo.com>
2022-05-30 15:10:57 -0700

    config to fix error

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <Head>
         <title>Home Page</title>
         <style>
-        @import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400&display=swap');
         </style>
 
       </Head>
